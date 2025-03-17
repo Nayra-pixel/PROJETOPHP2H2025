a62d3acb4ca4122f61384ee5d2e1edbe6aca59d9
Nayra_pixel <nayrasduarte@gmail.com>
2025-03-17 20:27:13 -0300

    trabalhando com variáveis

---
+++ quinto.php
@@ -0,0 +1,13 @@
+<?php 
+	$nome = "Kimberly";
+
+	function exibir(){ 
+	$nome = "Kika";
+
+	echo "Valor da variável dentro da função ".$nome; 
+	}
+
+	exibir(); 
+
+	echo "<br/> Valor da variável fora da função ".$nome; 
+?>
