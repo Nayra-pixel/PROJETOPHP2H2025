98210a06b8f77d383763a3870f66cb6e2648b090
Nayra-pixel <nayrasduarte@gmail.com>
2025-04-07 20:15:54 -0300

    function

---
+++ somar.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	//executando uma function
+	function somaValor(){
+
+		$num1 = 150;
+		$num2 = 220;
+
+		$resp = $num1 + $num2;
+
+		echo "A soma de num1 + num2 é: " . $resp;
+	}
+
+	//executando uma function
+
+	somaValor();
+
+ ?>
---
+++ somarsintetizado.php
@@ -0,0 +1,15 @@
+<?php 
+	
+	$num1 = 11;
+	$num2 = 11;
+
+	function somaValor($n1, $n2){
+
+		return($n1 + $n2);
+
+	}
+
+	echo "A soma dos valores é: " . somaValor($num1, $num2);
+
+
+ ?>
