df3413f727d5151a0bf2f84bd875e2f4ff5acd78
Nayra-pixel <nayrasduarte@gmail.com>
2025-04-28 19:22:34 -0300

    tabela ir

--- calculo_ir.php
+++ calculo_ir.php
@@ -1,41 +1,48 @@
 <?php 
    
-   $salbrut = 7500.00; 
+   $salbrut = 2500.00; 
 
    function ir($salbrut){
- 	GLOBAL $alq;
- 	if($salbrut >= 2428.81 && $salbrut <= 2826.65){
+ 	 GLOBAL $alq, $desc, $saliq;
+ 	 if($salbrut >= 2428.81 && $salbrut <= 2826.65){
  		$alq = 7.5;
  	 	$desc = $salbrut * (7.5 / 100);
  		$saliq = $salbrut - $desc;    
- 	}
+ 	 }
 
     elseif($salbrut >= 2826.66 && $salbrut <= 3751.05){
     	$alq = 15;
  		$desc = $salbrut * (15 / 100);
  		$saliq = $salbrut - $desc;
- 	}
- 	elseif($salbrut >= 3751.06 && $salbrut <= 4664.68){
+ 	 }
+ 	 elseif($salbrut >= 3751.06 && $salbrut <= 4664.68){
  		$alq = 22.5;
  		$desc = $salbrut * (22.5 / 100);
  		$saliq = $salbrut - $desc;
- 	}
- 	elseif($salbrut > 4664.68 ){
+ 	 }   
+ 	 elseif($salbrut > 4664.68 ){
  		$alq = 27.5;
  		$desc = $salbrut * (27.5 / 100);
  		$saliq = $salbrut - $desc;
- 	}else{
+ 	 }
+    else{
  		$alq = 0;
+      $desc = 0;
+      $saliq = $salbrut - $desc;
 
- 	}
+ 	 }
     
-     echo ("Salário Bruto: " . $salbrut . 
- 		"<br>Porcentagem de Desconto: " . $alq .
- 		"<br>Valor do Desconto: " . $desc .
- 		"<br>Salário Líquido a Receber: " . $saliq);
+      return($salbrut);
+      return($alq);
+      return($desc);
+      return($saliq);
 
    }
    
    ir($salbrut);
+   echo ("Salário Bruto: R$" . $salbrut .
+      "<br>Porcentagem de Desconto: " . $alq . "%" .
+      "<br>Valor do Desconto: R$" . $desc .
+      "<br>Salário Líquido a Receber: R$" . $saliq);
 
  ?>
