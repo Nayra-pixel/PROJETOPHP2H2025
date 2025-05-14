3fef54fad61fb4f4137b5fbe74dfd77d0fbe366c
Nayra-pixel <nayrasduarte@gmail.com>
2025-05-13 22:11:07 -0300

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$nome = array();
+
+	$nome[0] = 'Kim';
+	$nome[1] = 'Kika';
+	$nome[2] = 'Kiko';
+	$nome[3] = 'Kimberelo';
+
+
+	print_r($nome);
+	echo "<br>";
+	var_dump($nome);
+	echo "<br>";
+	
+ ?>
---
+++ veiculos.php
@@ -0,0 +1,9 @@
+<?php 
+
+	$carros = array('I30','Kwid','Punto','Uno',"Gol Quadrado");
+
+	$carros[] = 'Honda Fit';
+
+	var_dump($carros);
+
+ ?>
